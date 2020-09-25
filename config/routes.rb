436e8af9250113ef27b2fabb350cf8797a9dcc7f
sato310-sato <under_color27@yahoo.co.jp>
2020-09-25 17:52:41 +0900

    furima画像の中のディレクトリの移動

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   #どのモデルでユーザーログインしたいか
   devise_for :users
   root to: 'items#index'
-  resources :users, only: [:edit, :update]
+  # resources :users, only: [:edit, :update]
   resources :items, only: [:index]
 end
