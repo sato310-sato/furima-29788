2441a3d7fab13d0d3a943315fa6aec3a2e9d4c8f
sato310-sato <under_color27@yahoo.co.jp>
2020-09-25 10:45:11 +0900

    deviseの導入

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :comments
+  has_many :orders
+ 
+    # ニックネームが必須
+    validates :nickname, presence: true
+    # 苗字・名前が必須
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    #パスワードは二回入力する 
+    validates :password,confirmation: true
+    validates :password_confirmation, presence: true
+ 
+  
+    with_options presence: true, format: { with: /\A(?=.*?[a-z\d?=@])(?=.*?[\d])[a-z\d]+\z/i, message: 'メールアドレスを入力して下さい' } do
+    # emeailが必須、大文字小文字を区別しない一意、＠含む、
+    validates :email , uniqueness: true
+    # パスワードが必須、６文字以上
+    validates :password, length: { minimum: 6 }
+    end
+
+  # 全角入力
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  # パスワードは半角英数字
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+end
+
