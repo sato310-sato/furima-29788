1751f356c7f5fa63cafdc62d7edafc994d48b0b3
sato310-sato <under_color27@yahoo.co.jp>
2020-09-25 14:06:12 +0900

    userモデルのテストコードを記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.nickname}
-    first_name {Faker::Name.first_name}
+    nickname {Faker::Name.name}
     last_name {Faker::Name.last_name}
-    first_name_name {Faker::Name.first_name}
-    last_name_kana_name {Faker::Name.last_name_kana}
-    first_name_kananame {Faker::Name.first_name_kana}
+    first_name {Faker::Name.first_name}
+    last_name_kana {Faker::Name.last_kana_name}
+    first_name_kana {Faker::Name.first_kana_name}
     birthday {Faker::Date.birthday}
     email {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
-    password {password}
     password_confirmation {password}
   end
 end
