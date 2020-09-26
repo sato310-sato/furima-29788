862a8076c26973bd7e8682a579a76bae0025a0ec
sato310-sato <under_color27@yahoo.co.jp>
2020-09-26 11:47:02 +0900

    ユーザー登録画面の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,14 @@
 class ItemsController < ApplicationController
+
+  # ロクインして言うなくもindexとshowは閲覧できる
+  before_action :move_to_index, except: [:index, :show]
   def index
     
+  end
+# 未ログインの場合indexへリダイレクト
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
