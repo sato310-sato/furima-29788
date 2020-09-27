b49f027bfbd7135fa3d40bcda64950094c29c468
sato310-sato <under_color27@yahoo.co.jp>
2020-09-27 12:11:41 +0900

    passwordのテストコードを修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -70,9 +70,9 @@ RSpec.describe User, type: :model do
           expect(@user.errors.full_messages).to include("Password can't be blank")
         end
 
-        it 'passwordが5文字以下であれば登録できないこと' do
-          @user.password = '12345'
-          @user.password_confirmation = '12345'
+        it 'passwordが英数字5文字以下であれば登録できないこと' do
+          @user.password = '12kj4'
+          @user.password_confirmation = '12kj4'
           @user.valid?
           expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
         end
