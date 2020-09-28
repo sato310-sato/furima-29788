df5c0cc8c7c93464b78fb239ace44112bf6ceef7
sato310-sato <under_color27@yahoo.co.jp>
2020-09-28 21:02:09 +0900

    itemsのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   # resources :users, only: [:edit, :update]
-  resources :items, only: :index
+  resources :items, only: [:index, :edit, :new, :show]
   # oder作成後
   #resources :oder, only: [: , :]
 end
