08e5bedc90c038a67fd8579a2eb4b9bbf0b36fdc
sato310-sato <under_color27@yahoo.co.jp>
2020-10-03 12:20:19 +0900

    itemカラムの修正とバリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,18 +8,25 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_one :order
+  has_one_attached :image
 
   with_options presence: true do
     validates :name
-    validates :category
     validates :price
+    validates :category_id
     validates :item_text
-    validates :delivery_free
-    validates :product_condition
-    validates :delivery_source
-    validates :estimated_delivery
-    validates :user
+    validates :delivery_free_id
+    validates :product_condition_id
+    validates :delivery_source_id
+    validates :estimated_delivery_id
+    validates :image
   end
+
+  # validates :price, numericality: { greater_than: 299, less_than: 10000000 }
+  # 指定された値より等しいか大きい、小さい
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 10000000 }
+  @price_regex = /\A[0-9]+\z/
+  validates_format_of :price, with: @price_regex, allow_nil: true 
 
   validates :category_id, numericality: { other_than: 1 }
   validates :delivery_free_id, numericality: { other_than: 1 }
