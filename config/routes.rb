06807748906f6807485f77ee83d23eb573ab31d7
sato310-sato <under_color27@yahoo.co.jp>
2020-10-03 12:27:15 +0900

    createをルーティングに追記

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   # resources :users, only: [:edit, :update]
-  resources :items, only: [:index, :edit, :new, :show]
+  resources :items, only: [:index, :edit, :new, :show, :create]
   # oder作成後
   #resources :oder, only: [: , :]
 end
