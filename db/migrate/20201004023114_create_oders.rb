64d32986da0f68aed465bebaa757a43d5c345885
sato310-sato <under_color27@yahoo.co.jp>
2020-10-04 12:10:46 +0900

    ordersテーブルの作成

---
+++ db/migrate/20201004023114_create_oders.rb
@@ -0,0 +1,9 @@
+class CreateOders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :oders do |t|
+      t.references :item, foreign_key: true
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
