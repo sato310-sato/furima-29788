7bd97034be0df71e91dc7b463f68f2c943e4b58b
sato310-sato <under_color27@yahoo.co.jp>
2020-10-08 20:55:50 +0900

    orderのルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: "items#index"
   # resources :users, only: [:edit, :update]
   resources :items, only: [:index, :edit, :new, :show, :create,:destroy,:update] do
-    resources :comment, only: [:create]
-    resources :oder, only: [:create]
-    # resources :shopping_addresses, only: [:index]
+    # resources :comment, only: [:create]
+    resources :orders, only: [:index, :create]
   end
+  
 end
