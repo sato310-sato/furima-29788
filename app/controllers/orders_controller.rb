b467528bb38709e93e588dcfd54004f88eafd2f4
sato310-sato <under_color27@yahoo.co.jp>
2020-10-08 20:58:11 +0900

    orderコントローラー作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,34 @@
+class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @price = UserPrice.new(price_params)
+    # binding.pry
+    if @price.valid?
+      pay_item
+      @price.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def price_params
+    # binding.pry
+    params.permit(:postal_code, :prefectural_id, :city, :house_number, :building, :phone_number, :item_id, :token).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: price_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
